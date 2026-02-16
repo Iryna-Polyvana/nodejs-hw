51de1fdaba89f9f04e063eb86364297b713aeef5
Iryna Polyvana <irynapolyvana@gmail.com>
2026-02-16 13:03:49 +0100

    fix: correct validation in note model and improve errorHandler logic

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -8,10 +8,8 @@ export const errorHandler = (err, req, res, next) => {
       message: err.message || err.name,
     });
   };
-  const isProd = process.env.NODE_ENV === "production";
+
   res.status(500).json({
-    message: isProd
-      ? "Something went wrong. Please try again later."
-      : err.message,
+    message: err.message,
   });
 };
