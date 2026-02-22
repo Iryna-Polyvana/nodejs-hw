ca78c998280ddececa3487370c13eef7f3dd9c7a
Iryna Polyvana <irynapolyvana@gmail.com>
2026-02-22 19:13:47 +0100

    feat: implement auth service with session creation and cookies setup

--- src/models/session.js
+++ src/models/session.js
@@ -12,13 +12,3 @@ const sessionSchema = new Schema(
 );
 
 export const Session = model('Session', sessionSchema);
-
-
-
-// У файлі src / models / session.js створіть модель Session із такими властивостями:
-// userId — тип Schema.Types.ObjectId, обов’язкове, посилання на модель User;
-// accessToken — рядок, обов’язкове;
-// refreshToken — рядок, обов’язкове;
-// accessTokenValidUntil — тип Date, обов’язкове;
-// refreshTokenValidUntil — тип Date, обов’язкове.
-// Для автоматичного створення полів createdAt та updatedAt, використовуйте параметр timestamps: true при створенні моделі.
