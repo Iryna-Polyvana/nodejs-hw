4966d738220021f03d2da6eff80cd1e9548b78dd
Iryna Polyvana <irynapolyvana@gmail.com>
2026-02-22 18:36:24 +0100

    feat: add Session model and time constants

--- src/models/session.js
+++ src/models/session.js
@@ -1,1 +1,24 @@
 import { model, Schema } from "mongoose";
+
+const sessionSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    accessToken: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+    accessTokenValidUntil: { type: Date, required: true },
+    refreshTokenValidUntil: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Session = model('Session', sessionSchema);
+
+
+
+// У файлі src / models / session.js створіть модель Session із такими властивостями:
+// userId — тип Schema.Types.ObjectId, обов’язкове, посилання на модель User;
+// accessToken — рядок, обов’язкове;
+// refreshToken — рядок, обов’язкове;
+// accessTokenValidUntil — тип Date, обов’язкове;
+// refreshTokenValidUntil — тип Date, обов’язкове.
+// Для автоматичного створення полів createdAt та updatedAt, використовуйте параметр timestamps: true при створенні моделі.
